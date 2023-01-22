99f8e105d87a7d4a0e15e6a9e071d6414d8cd74b
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-01-23 01:39:37 +0530

    Completed Experience Screen

--- src/components/experiences/ExperienceScreen.js
+++ src/components/experiences/ExperienceScreen.js
@@ -29,10 +29,13 @@ const mainContainerVariant = {
 
 const cardVariant = {
   hidden: {
-    opacity: 0,
+
   },
   visible: {
-    opacity: 1,
+    transition: {
+      staggerChildren: 0,
+      duration: 0
+    },
   },
   transition: {
 
@@ -42,20 +45,91 @@ const cardVariant = {
   },
 }
 
-// const contentVariant = {
-//   hidden: {
+const dateVariant = {
+  hidden: {
+    opacity: 0,
+    scaleX: 0.1,
+    // x: '10%',
+  },
+  visible: {
+    opacity: 1,
+    scaleX: 1,
+    // x: '0%',
+    transition: {
+      type: 'tween',
+      duration: 0.5,
+    },
+  },
+  transition: {
 
-//   },
-//   visible: {
+  },
+  exit: {
 
-//   },
-//   transition: {
+  },
+}
 
-//   },
-//   exit: {
+const timelineVariant = {
+  hidden: {
+    // opacity: 0,
+    height: 0,
+  },
+  visible: {
+    // opacity: 1,
+    height: '100%',
+    transition: {
+      type: 'tween',
+      duration: 0.5,
+    },
+  },
+  transition: {
 
-//   },
-// }
+  },
+  exit: {
+
+  },
+}
+
+const squareVariant = {
+  hidden: {
+    opacity: 0,
+    scale: 0,
+  },
+  visible: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: 'tween',
+      duration: 0.5,
+    },
+  },
+  transition: {
+
+  },
+  exit: {
+
+  },
+}
+
+const contentVariant = {
+  hidden: {
+    opacity: 0,
+    x: '10%',
+  },
+  visible: {
+    opacity: 1,
+    x: '0%',
+    transition: {
+      type: 'tween',
+      duration: 0.5
+    },
+  },
+  transition: {
+
+  },
+  exit: {
+
+  },
+}
 
 const getDate = (date) => {
   if (date === '-' || date === 'Ongoing') return date;
@@ -105,19 +179,19 @@ const ExperienceScreen = () => {
             >
               <div className='w-2/5 h-24 md:h-28 lg:h-32 flex flex-col self-center items-center justify-center text-white-sec xs:text-sm 2xs:text-xs text-2xs lg:text-base'>
                 <div className='mr-0.5 '>{getDate(experience.startDate)}</div>
-                <div className='mr-0.5 leading-none w-full md:w-4/5 lg:w-3/5 my-1 h-0.5 bg-yellow'></div>
+                <motion.div variants={dateVariant} className='mr-0.5 leading-none w-full md:w-4/5 lg:w-3/5 my-1 h-0.5 bg-yellow'></motion.div>
                 <div className='mr-0.5 '>{getDate(experience.endDate)}</div>
               </div>
 
               <div className='w-1/4 self-stretch relative flex flex-col items-center justify-center'>
-                <div className={(i === 0 ? 'rounded-t-full' : (i === Experiences.length - 1 ? 'rounded-b-full' : '')) + ` absolute h-full w-1 bg-yellow -z-10`}></div>
-                <div className='h-4 md:h-5 lg:h-6 aspect-square rounded-sm bg-black border-2 border-solid border-yellow grid place-items-center box-content'>
-                  <div className='h-3/5 aspect-square bg-yellow rounded-sm'></div>
+                <motion.div variants={timelineVariant} className={(i === 0 ? 'rounded-t-full' : (i === Experiences.length - 1 ? 'rounded-b-full' : '')) + ` absolute top-0 h-full w-1 bg-yellow`}></motion.div>
+                <div className='h-4 md:h-5 lg:h-6 aspect-square rounded-sm bg-black border-2 border-solid border-yellow grid place-items-center box-content z-10'>
+                  <motion.div variants={squareVariant} className='h-3/5 aspect-square bg-yellow rounded-sm'></motion.div>
                 </div>
               </div>
 
-              <div className='w-full flex flex-row items-start ml-2 bg-black-op rounded-sm my-1 py-1 cursor-pointer'>
-                <img className='mt-2 h-12 md:h-14 lg:h-[4.5rem] object-cntain border-2 border-solid border-black rounded-sm px-1 md:px-1.5 lg:px-2' src={experience.iconLink} alt="icon-not-found" />
+              <motion.div layout variants={contentVariant} className='w-full flex flex-row items-start ml-2 bg-black-op rounded-sm my-1 py-1 cursor-pointer'>
+                <img className='mt-2 h-12 md:h-14 lg:h-[4.5rem] object-contain border-2 border-solid border-black rounded-sm px-1 md:px-1.5 lg:px-2.5' src={experience.iconLink} alt="icon-not-found" />
                 <div className='self-stretch w-0.5 rounded-full flex flex-col justify-center'><div className='h-4/5 w-full bg-yellow'></div></div>
                 <div className='flex-grow flex flex-col px-2 py-2 overflow-x-hidden'>
                   <div className='text-yellow text-base md:text-lg lg:text-xl xl:text-2xl break-words leading-none mb-1'>{experience.name}</div>
@@ -127,7 +201,7 @@ const ExperienceScreen = () => {
                     <span className=''>&#x2022;{experience.type}</span>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             </motion.div>
           )
         })}
